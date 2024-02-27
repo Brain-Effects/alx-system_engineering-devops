1435555016a6bad6e416c33d24e36f15b1f41c67
Braib-Effects <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@github.com>
2024-02-27 09:12:08 -0800

    Manage my process

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -11,11 +11,11 @@
  */
 int infinite_while(void)
 {
-    while (1)
-    {
-        sleep(1);
-    }
-    return (0);
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
 }
 
 /**
@@ -25,21 +25,21 @@ int infinite_while(void)
  */
 int main(void)
 {
-    pid_t pid;
-    int i;
+	pid_t pid;
+	int i;
 
-    for (i = 0; i < 5; i++)
-    {
-        pid = fork();
-        if (pid > 0)
-        {
-            printf("Zombie process created, PID: %d\n", pid);
-        }
-        else
-        {
-            exit(0);
-        }
-    }
+	for (i = 0; i < 5; i++)
+	{
+	pid = fork();
+	if (pid > 0)
+	{
+		printf("Zombie process created, PID: %d\n", pid);
+	}
+	else
+		{
+		exit(0);
+		}
+	}
 
-    return (infinite_while());
+	return (infinite_while());
 }
