ddebecc014c1bbedb4b926070148b25518904eb8
Brain-Effects <braineffects@github.com>
2024-08-31 01:10:14 -0700

    Subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,6 +3,7 @@
 Module to query the number of subscribers from a subreddit
 """
 import requests
+import sys
 
 
 def number_of_subscribers(subreddit):
@@ -13,18 +14,18 @@ def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'myAPI/0.0.1'}
     response = requests.get(url, headers=headers, allow_redirects=False)
-
+    
     if response.status_code == 200:
         data = response.json()
-        return data['data']['subscribers']
+        return data['data'].get('subscribers', 0)
     else:
-        print(f"Error: Received status code {response.status_code}")
-        print(f"Response content: {response.content}")
         return 0
 
+
 if __name__ == '__main__':
-    import sys
+    number_of_subscribers = __import__('0-subs').number_of_subscribers
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
     else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+        subscribers = number_of_subscribers(sys.argv[1])
+        print(f"Number of subscribers: {subscribers}")
