cbfb13f2570a5bf2ee661459b197b76f891af44f
Brain-Effects <braineffects@github.com>
2024-08-30 23:54:19 -0700

    Subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,21 +7,22 @@ import requests
 
 def number_of_subscribers(subreddit):
     """
-    Queries the Reddit API and returns the number of
-    subscribers for a given subreddit.
-    If the subreddit is invalid, returns 0.
+    Queries the Reddit API and returns the number of subscribers for a
+    given subreddit.
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Custom User-Agent'}
+    headers = {'User-Agent': 'Python: subreddit.subscriber.counter: v1.0\
+            (by /u/yourusername)'}
+    
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
+        print(f"Response status code: {response.status_code}")
         if response.status_code == 200:
             data = response.json()
+            print(f"Response JSON data: {data}")
             return data.get('data', {}).get('subscribers', 0)
-        elif response.status_code == 404:
-            print("Invalid subreddit")
-            return 0
         else:
             return 0
-    except requests.RequestException:
+    except requests.RequestException as e:
+        print(f"Request failed: {e}")
         return 0
