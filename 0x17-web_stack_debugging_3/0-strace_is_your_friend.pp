32082f79087b36f46d677d8b68572e5b4d62a87f
Brain-Effects <braineffects@github.com>
2024-09-01 14:55:08 -0700

    Strace is your friend

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,20 @@
+# This Puppet manifest fixes the Apache 500 error by ensuring the
+# required directory exists with correct permissions
+file { '/var/www/html':
+  ensure  => 'directory',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0755',
+  recurse => true,
+}
+
+exec { 'apache_configtest':
+  command     => '/usr/sbin/apachectl configtest',
+  refreshonly => true,
+  notify      => Exec['restart_apache'],
+}
+
+exec { 'restart_apache':
+  command     => '/usr/sbin/systemctl restart apache2',
+  refreshonly => true,
+}
