084aa883fdaf242c5b872425c7dd6004e5407307
Brain-Effects <braineffects@github.com>
2024-09-01 15:03:34 -0700

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -7,20 +7,3 @@ file { '/var/www/html':
   mode    => '0755',
   recurse => true,
 }
-
-exec { 'apache_configtest':
-  command     => '/usr/sbin/apachectl configtest',
-  refreshonly => true,
-  notify      => Exec['restart_apache'],
-}
-
-exec { 'restart_apache':
-  command     => '/usr/sbin/systemctl restart apache2',
-  refreshonly => true,
-}
-
-service { 'apache2':
-  ensure     => 'running',
-  enable     => true,
-  subscribe  => File['/var/www/html'],
-}
