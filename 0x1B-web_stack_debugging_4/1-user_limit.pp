10560666ca5d181835cdc4071b6f2ef222a79f35
Brain-Effects <braineffects@github.com>
2024-09-04 08:48:24 -0700

    User limits

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,13 +1,13 @@
 # This Puppet script increases the file descriptor limits for the holberton user
 
 exec { 'holberton hard':
-  path    => ['/usr/bin/'],
-  command => "sudo sed -i 's/^holberton hard /holberton hard nofile 2000/g' /etc/security/limits.conf",
+  path     => ['/usr/bin/'],
+  command  => "sudo sed -i 's/^holberton hard /holberton hard nofile 2000/g' /etc/security/limits.conf",
   provider => 'shell',
 }
 
 exec { 'holberton soft':
-  path    => ['/usr/bin/'],
-  command => "sudo sed -i 's/^holberton soft /holberton soft nofile 2000/g' /etc/security/limits.conf",
+  path     => ['/usr/bin/'],
+  command  => "sudo sed -i 's/^holberton soft /holberton soft nofile 2000/g' /etc/security/limits.conf",
   provider => 'shell',
 }
